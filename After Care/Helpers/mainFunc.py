ac9a921ccb729527b8e0d32e88dca8de9ea5ace9
Eytan <73502334+BiggieFudge@users.noreply.github.com>
2023-08-24 14:51:12 +0300

    Update mainFunc.py
    to work with new json
    add github download

--- After Care/Helpers/mainFunc.py
+++ After Care/Helpers/mainFunc.py
@@ -6,10 +6,12 @@ import requests
 import os
 import json
 
+
 #jsonFile = json.load("appsLinkDict.json")
 with open('appsLinkDict.json', 'r') as f:
-  data = json.load(f)
-websiteScrape = 'https://www.apkmirror.com/'
+    data = json.load(f)
+download_dir = os.getcwd() + r'\tempks'
+#websiteScrape = 'https://www.apkmirror.com/'
 # appLinkDict = {'waze':'apk/waze/waze-gps-maps-traffic-alerts-live-navigation/',
 #                'chrome':'apk/google-inc/chrome/',
 #                'firefox':'apk/mozilla/firefox/',
@@ -43,32 +45,52 @@ websiteScrape = 'https://www.apkmirror.com/'
 
 def mainScrape(listOfApps,beta):
     for App in listOfApps:
-        downloadWebSite,downloadLink = searchForApp(App)
-        if downloadWebSite == 'https://www.apkmirror.com/':
-            downloadAPK(downloadWebSite + downloadLink,beta)
+        #downloadWebSite,downloadLink = searchForApp(App)
+        download = data[App]
+        #downloadListTemp = download.split('/')
+        #downloadparse = urlparse(download)
+        #downloadWebSite =  downloadparse.hostname
+        if download.startswith('https://www.apkmirror.com/'):
+            downloadAPKMirror(download,beta)
+        elif download.startswith('https://github.com/'):
+            downloadGitHub(download)
 
 
 
-def downloadAPK(downloadLink,beta):
+def initSelenium():
     #################Init######################
     local_state = {
         "dns_over_https.mode": "secure",
         "dns_over_https.templates": "https://dns.adguard-dns.com/dns-query",
     }
     options = Options()
-    #options.add_argument("--headless")
+    # options.add_argument("--headless")
     os.getcwd()
-    download_dir =os.getcwd() + r'\tempks'
 
-    prefs =  {"profile.default_content_settings.popups": 0,
-        "download.default_directory":download_dir, ### Set the path accordingly
-        "download.prompt_for_download": False, ## change the downpath accordingly
-        "download.directory_upgrade": True}
+
+    prefs = {"profile.default_content_settings.popups": 0,
+             "download.default_directory": download_dir,  ### Set the path accordingly
+             "download.prompt_for_download": False,  ## change the downpath accordingly
+             "download.directory_upgrade": True}
     options.add_experimental_option("prefs", prefs)
 
     options.add_experimental_option('localState', local_state)
     driver = webdriver.Chrome(options=options)
+    return driver
 
+def downloadGitHub(downloadLink):
+    driver = initSelenium()
+    driver.get(downloadLink)
+    driver.execute_script(f"window.scrollTo(0, 800);")
+    time.sleep(1)
+    downloadHREF = driver.find_element(By.PARTIAL_LINK_TEXT,"apk").get_attribute("href")
+    fileNameVersion = downloadHREF.split('/')[-1]
+    session = requests.Session()
+    response = session.get(downloadHREF)
+    saveFile(fileNameVersion,response,driver)
+
+def downloadAPKMirror(downloadLink,beta):
+    driver,download_dir = initSelenium()
 
     driver.get(downloadLink)
     seleniumUserAgent = driver.execute_script("return navigator.userAgent")
@@ -95,8 +117,10 @@ def downloadAPK(downloadLink,beta):
     driver.execute_script(f"window.scrollTo(0, 800);")
     driver.find_element(By.XPATH, '//*[@id="downloads"]/div/div/div[2]/div[5]/a').click()
     driver.execute_script(f"window.scrollTo(0, 300);")
-    driver.find_element(By.XPATH, '//*[@id="file"]/div[1]/div[2]/div/a').click()
-
+    try:
+        driver.find_element(By.XPATH, '//*[@id="file"]/div[1]/div[2]/div/a').click()
+    except:
+        driver.find_element(By.XPATH,'/html/body/div[2]/div/div[1]/article/div[2]/div[3]/div[1]/div[2]/div[2]/div/a').click()
     downloadlink = driver.find_element(By.XPATH,
                                        '/html/body/div[2]/div/div[1]/article/div[2]/div/div/div[1]/p[2]/span/a').get_attribute(
         "href")
@@ -109,25 +133,35 @@ def downloadAPK(downloadLink,beta):
     for cookie in cookies:
         session.cookies.set(cookie['name'], cookie['value'])
     response = session.get(downloadlink)
-    #check if response is succeful and only then continue
     latest_download = max(os.listdir(download_dir), key=lambda x: os.path.getctime(os.path.join(download_dir, x)))
-    local_file_path = "apks/" + fileNameVersion + ".apk"
+    saveFile(fileNameVersion + ".apk",response,driver)
+    os.remove(os.path.join(download_dir, latest_download))
+    # Close the WebDriver session when done
+
+
+def saveFile(fileNameVersion,response,driver):
+    #check if response is succeful and only then continue
+    if fileNameVersion == "app-release.apk":
+        fileNameVersion = driver.find_element(By.XPATH,"""//*[@id="repo-content-pjax-container"]/div/div/div/div[1]/div[1]/div[1]/div[1]/h1""").text + ".apk"
+    local_file_path = "apks/" + fileNameVersion
     with open(local_file_path, 'wb') as file:
         file.write(response.content)
-    os.remove(os.path.join(download_dir ,latest_download))
-    print('succefully downloaded ' + fileNameVersion)
-    # Close the WebDriver session when done
     driver.quit()
 
-def searchForApp(nameOfApp):
-    for website in data['websites']:
-        if website['apps'][nameOfApp]:
-            return website['url'],website['apps'][nameOfApp]
+    print('succefully downloaded ' + fileNameVersion)
+
+# def searchForApp(nameOfApp):
+#     for website in data['websites']:
+#         try:
+#             if website['apps'][nameOfApp]:
+#                 return website['url'],website['apps'][nameOfApp]
+#         except:
+#             continue
 
 
 
 
 if __name__ == "__main__":
-    listOfApps = ["youtube"]
+    listOfApps = ["droidify","vimusic","newpipexsponserblock","adaway","rethink"]
     beta = True
     mainScrape(listOfApps, beta)
